25ab2f4016a80bd14c86921e6198cb1440a740de
Elkin Moreno Vélez <itmedellin2018@gmail.com>
2019-06-02 00:34:03 -0500

    Revisando parrafo p

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Mi primera aplicación</h1>
-        <p>
-          Esta es mi primera aplicación en React, está padrisimo!</p>
+        <p>Esta es mi primera aplicación en React, está padrisimo!</p>
       </header>
     </div>
   );
