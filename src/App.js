ef5fdda716e3beadd5726c8a844b936bd862678a
Elkin Moreno Vélez <itmedellin2018@gmail.com>
2019-06-02 09:38:17 -0500

    Ajustando App

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React from 'react';
 function App() {
   return (
     <div>
-        <h1>Mi primera aplicación</h1>
-        <p>Esta es mi primera aplicación en React, está padrisimo!</p>
+      <h1>Mi primera aplicación</h1>
+      <p>Esta es mi primera aplicación en React, está padrísimo!</p>
     </div>
   );
 }
 
 export default App;
+
