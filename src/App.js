3b7f25db2947ef9673253c6c41367c8fe8b88106
Elkin Yesid Moreno Velez <elkin.moreno@mvm.com.co>
2019-06-01 21:52:47 -0500

    mi primer aplicación React

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mi primera aplicación</h1>
+        <p>
+          Esta es mi primera aplicación en React, está padrisimo!. </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
