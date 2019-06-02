051f83c186c9f2102dbe11089424834f815dc1a0
Elkin Moreno Vélez <itmedellin2018@gmail.com>
2019-06-02 00:36:51 -0500

    Revisando parrafo p

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <h1>Mi primera aplicación</h1>
         <p>Esta es mi primera aplicación en React, está padrisimo!</p>
-      </header>
     </div>
   );
 }
